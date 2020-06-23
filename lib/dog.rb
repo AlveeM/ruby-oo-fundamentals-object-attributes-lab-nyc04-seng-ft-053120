063ffee4f8e53a875d76ab3fc40661b3f642585e
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-23 01:37:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Person 
-  def initialize(name)
+  def initialize(name, job)
     @name = name
+    @job = job
   end 
   
   def name 
@@ -9,5 +10,13 @@ class Person
   
   def name=(new_name)
     @name = new_name
+  end 
+  
+  def job 
+    @job 
+  end 
+  
+  def job=(new_job) 
+    @job = new_job 
   end 
 end 
